6a4cbdb7691e7f625880c04a85f4a172629f92cc
nitish2202 <nitish.2202@gmail.com>
2021-01-24 22:40:39 +0530

    handling UserData form submission

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -66,9 +66,14 @@ class ContactData extends Component {
     orderHandler = (event) => {
         event.preventDefault();
         this.setState({ loading: true })
+        const formData = {};
+        for(let formElementIdentifier in this.state.orderForm){
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
         const order = {
             ingredients: this.props.ingredients,
-            price: this.props.price
+            price: this.props.price,
+            orderData: formData
         }
 
         axios.post('/orders.json', order)
@@ -105,7 +110,7 @@ class ContactData extends Component {
                 config: this.state.orderForm[key]
             });
         }
-        let form = (<form>
+        let form = (<form onSubmit={this.orderHandler}>
             {formElementsArray.map(formElement => (
                 <Input 
                 key={formElement.id}
@@ -114,7 +119,7 @@ class ContactData extends Component {
                 value={formElement.config.value}
                 changed={(event)=>this.inputChangedHandler(event, formElement.id)} />
             ))}
-            <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+            <Button btnType="Success">ORDER</Button>
         </form>);
         if(this.state.loading){
             form = <Spinner />
