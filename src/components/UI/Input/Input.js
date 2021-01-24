b63e9632253058ef0a6fd295955d2fe8861593e4
nitish2202 <nitish.2202@gmail.com>
2021-01-24 22:24:57 +0530

    added two way binding in UserData form

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -6,13 +6,22 @@ const input = (props) => {
 
     switch(props.elementType){
         case('input'):
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value}/>;
+            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed}/>;
             break;
         case('textarea'):
-            inputElement = <textarea className={classes.InputElement} {...props.elementConfig} value={props.value}/>;
+            inputElement = <textarea className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed}/>;
+            break;
+        case('select'):
+            inputElement = (<select className={classes.InputElement} value={props.value} onChange={props.changed}>
+                {props.elementConfig.options.map(option => (
+                    <option key={option.value} value={option.value}>
+                        {option.displayValue}
+                    </option>
+                ))}
+            </select>);
             break;
         default:
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value}/>;
+            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed}/>;
     }
     return(
         <div className={classes.Input}>
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -83,12 +83,37 @@ class ContactData extends Component {
             });
     }
 
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        };
+
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        };
+
+        updatedFormElement.value = event.target.value;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        this.setState({ orderForm: updatedOrderForm });
+    }
+
     render() {
+        const formElementsArray = [];
+        for(let key in this.state.orderForm){
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
         let form = (<form>
-            <Input elementType="" elementConfig="" value=""/>
-            <Input inputtype="input" type="email" name="email" placeholder="Your Mail" />
-            <Input inputtype="input" type="text" name="street" placeholder="Street" />
-            <Input inputtype="input" type="text" name="postal" placeholder="Postal Code" />
+            {formElementsArray.map(formElement => (
+                <Input 
+                key={formElement.id}
+                elementType={formElement.config.elementType} 
+                elementConfig={formElement.config.elementConfig} 
+                value={formElement.config.value}
+                changed={(event)=>this.inputChangedHandler(event, formElement.id)} />
+            ))}
             <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
         </form>);
         if(this.state.loading){
