40f96bef8eb4138f8e2582b60b7df58720ac150c
nitish2202 <nitish.2202@gmail.com>
2021-02-28 22:10:12 +0530

    adding logout functionality

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,21 @@ export const authFail = (error) => {
     };
 };
 
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    };
+};
+
+export const checkAuthTimeout = (expirationTime) => {
+    return dispatch => {
+        setTimeout(() => {
+            //call logout action
+            dispatch(logout());
+        }, expirationTime * 1000);
+    };
+};
+
 export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
@@ -39,6 +54,7 @@ export const auth = (email, password, isSignup) => {
             .then( response => {
                 console.log(response);
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn));
             })
             .catch( err => {
                 dispatch(authFail(err.response.data.error));
