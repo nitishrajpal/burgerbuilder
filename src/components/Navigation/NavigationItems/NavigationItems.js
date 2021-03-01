db72269c92a453c214ef37d42eb333bb2a30547a
nitish2202 <nitish.2202@gmail.com>
2021-03-01 23:50:41 +0530

    handling redirection base on authentication status

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,7 +5,7 @@ import NavigationItem from './NavigationItem/NavigationItem';
 const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/">Burger Builder</NavigationItem>
-        <NavigationItem link ="/orders">Orders</NavigationItem>
+        {props.isAuthenticated? <NavigationItem link ="/orders">Orders</NavigationItem> : null}
         {props.isAuthenticated ?
             <NavigationItem link ="/logout">Logout</NavigationItem> :
             <NavigationItem link ="/auth">Authenticate</NavigationItem>
--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
@@ -115,8 +116,14 @@ class Auth extends Component {
             );
         }
 
+        let authRedirect = null;
+        if(this.props.isAuthenticated){
+            authRedirect = <Redirect to="/" />
+        }
+
         return (
             <div className={classes.Auth}>
+                {authRedirect}
                 {errorMessage}
                 <form onSubmit={this.submitHandler}>
                     {form}
@@ -131,7 +138,8 @@ class Auth extends Component {
 const mapStateToProps = state => {
     return {
         loading: state.auth.loading,
-        error: state.auth.error
+        error: state.auth.error,
+        isAuthenticated: state.auth.token !== null
     };
 };
 
