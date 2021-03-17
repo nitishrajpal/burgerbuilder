eabe58c703ea358d6888c7a54d4014b74c89c839
nitish2202 <nitish.2202@gmail.com>
2021-03-18 00:21:45 +0530

    removed unnecessary loggings

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -6,21 +6,6 @@ import ContactData from '../../containers/Checkout/ContactData/ContactData';
 
 class Checkout extends Component {
 
-    // componentWillMount () {
-    //     const query = new URLSearchParams(this.props.location.search);
-    //     const ingredients = {};
-    //     let price = 0;
-    //     for(let param of query.entries()){
-    //         if(param[0]==='price'){
-    //             price=param[1];
-    //         }
-    //         else{
-    //             ingredients[param[0]] = +param[1];
-    //         }
-    //     }
-    //     this.setState({ ingredients: ingredients, totalPrice: price });
-    // }
-
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
     }
