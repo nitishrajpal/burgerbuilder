575bf89d22ea62d705e0d42fa8d1213fd0353381
nitish2202 <nitish.2202@gmail.com>
2021-03-18 00:18:18 +0530

    restricting redux dev tools to development environment only

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import thunk from 'redux-thunk';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
