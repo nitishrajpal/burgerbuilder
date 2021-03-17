8994a51214f212d1922a0b9a302b575fb528c51e
nitish2202 <nitish.2202@gmail.com>
2021-03-17 23:53:31 +0530

    code refactoring removed checkValidaty function duplicacy

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,7 +7,7 @@ import classes from './ContactData.css';
 import Input from '../../../components/UI/Input/Input';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
-import { updateObject } from '../../../shared/utility';
+import { updateObject, checkValidity } from '../../../shared/utility';
 
 class ContactData extends Component {
 
@@ -96,22 +96,6 @@ class ContactData extends Component {
         formIsValid: false
     }
 
-    checkValidity(value, rules){
-        let isValid = true;
-
-        if(rules.required){
-            isValid = value.trim() !== '' && isValid;
-        }
-        if(rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid
-        }
-        if(rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid
-        }
-
-        return isValid;
-    }
-
     orderHandler = (event) => {
         event.preventDefault();
         const formData = {};
@@ -130,7 +114,7 @@ class ContactData extends Component {
     inputChangedHandler = (event, inputIdentifier) => {
         const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier], {
             value: event.target.value,
-            valid: this.checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
+            valid: checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
             touched: true
         });
 
--- src/shared/utility.js
+++ src/shared/utility.js
@@ -4,3 +4,19 @@ export const updateObject = (oldObject, updateProperties) => {
         ...updateProperties
     };
 };
+
+export const checkValidity = (value, rules) => {
+    let isValid = true;
+
+    if(rules.required){
+        isValid = value.trim() !== '' && isValid;
+    }
+    if(rules.minLength) {
+        isValid = value.length >= rules.minLength && isValid
+    }
+    if(rules.maxLength) {
+        isValid = value.length <= rules.maxLength && isValid
+    }
+
+    return isValid;
+}
