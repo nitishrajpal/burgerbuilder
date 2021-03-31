bb704e99df0f4aae2d9cbb221d43ba0310d1acec
nitish2202 <nitish.2202@gmail.com>
2021-03-31 23:42:10 +0530

    unit test for logout button

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -23,4 +23,9 @@ describe('<NavigationItems />', () => {
         wrapper.setProps({ isAuthenticated: true });
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
+
+    it('should render three <NavigationItem /> if authenticated', () => {
+        wrapper.setProps({ isAuthenticated: true });
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
+    });
 });
