b104bea35d90d121adb7d85f60fdc7f1e562b219
nitish2202 <nitish.2202@gmail.com>
2021-04-03 18:58:19 +0530

    handling more actions with redux-saga

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -32,13 +32,17 @@ export const logout = () => {
     };
 };
 
+export const logoutSucceed = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    }
+};
+
 export const checkAuthTimeout = (expirationTime) => {
-    return dispatch => {
-        setTimeout(() => {
-            //call logout action
-            dispatch(logout());
-        }, expirationTime * 1000);
-    };
+    return {
+        type: actionTypes.AUTH_CHECK_TIMEOUT,
+        expirationTime: expirationTime
+    }
 };
 
 export const auth = (email, password, isSignup) => {
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -14,6 +14,7 @@ export {
 export {
     auth,
     logout,
+    logoutSucceed,
     setAuthRedirectPath,
     authCheckState
 } from './auth';
